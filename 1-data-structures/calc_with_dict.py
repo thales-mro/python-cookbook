8d2bc16d3b2cce8b14ef2cadd3c4292373138c1b
Thales Mateus Rodrigues Oliveira <ra148051@students.ic.unicamp.br>
2019-12-18 11:14:26 -0300

    Added 1.8 algorithm

---
+++ 1-data-structures/calc_with_dict.py
@@ -0,0 +1,19 @@
+def main():
+    stocks = {
+        'ACME': 45.23,
+        'AAPL': 612.78,
+        'IBM': 205.55,
+        'FB': 37.2,
+        'HPQ': 10.75,
+    }
+
+    min_price = min(zip(stocks.values(), stocks.keys()))
+    max_price = max(zip(stocks.values(), stocks.keys()))
+    print(min_price, max_price)
+
+    # it is equivalent to
+    min_val = stocks[min(stocks, key=lambda k: stocks[k])]
+
+
+if __name__ == "__main__":
+    main()
