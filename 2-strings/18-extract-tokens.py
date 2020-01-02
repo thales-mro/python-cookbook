3931058bba20bef7112a0713eac1b41d851e02a9
Thales Mateus Rodrigues Oliveira <thalesteng@gmail.com>
2020-01-02 17:26:57 -0300

    Added 2.18 and 2.19 recipes (tokens and parsers)

---
+++ 2-strings/18-extract-tokens.py
@@ -0,0 +1,40 @@
+import re
+from collections import namedtuple
+
+Token = namedtuple('Token', ['type', 'value'])
+
+def generate_tokens(pat, text):
+    scanner = pat.scanner(text)
+    for m in iter(scanner.match, None):
+        yield Token(m.lastgroup, m.group())
+
+def main():
+    text = 'foo = 23 + 42 * 10'
+
+    NAME = r'(?P<NAME>[a-zA-Z_][a-zA-Z_0-9]*)'
+    NUM = r'(?P<NUM>\d+)'
+    PLUS = r'(?P<PLUS>\+)'
+    TIMES = r'(?P<TIMES>\*)'
+    EQ = r'(?P<EQ>=)'
+    WS = r'(?P<WS>\s+)'
+
+    master_pat = re.compile('|'.join([NAME, NUM, PLUS, TIMES, EQ, WS]))
+
+    scanner = master_pat.scanner('foo = 42')
+    print(scanner.match())
+    print(scanner.match())
+    print(scanner.match())
+    print(scanner.match())
+    print(scanner.match())
+
+    for tok in generate_tokens(master_pat, 'foo =  42'):
+        print(tok)
+
+    print("Filtering some tokens:")
+    tokens = (tok for tok in generate_tokens(master_pat, text) if tok.type != 'WS')
+    for tok in tokens:
+        print(tok)
+
+
+if __name__ == "__main__":
+    main()
